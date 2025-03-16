e9e32ba8258e3ac1c7903faa5f7c5690c8386f62
mika <strarti@bk.ru>
2025-03-16 20:08:08 +0600

    update

--- src/App.js
+++ src/App.js
@@ -6,73 +6,74 @@ import CalcBtns from './components/CalcBtns/CalcBtns';
 function App() {
   const [theme, setTheme] = useState('dark');
   const [inputValue, setInputValue] = useState('')
-  const [operator, setOperator] = useState(null)
+  const [values, setValue] = useState([''])
+  const [operators, setOperator] = useState([])
   const toggleTheme = (theme) => {setTheme(theme)};
 
-  const isArithmetic = (str) => /^[+\-*/]$/.test(str);
-  const isNum = (str) => /^[-0-9]*(\.[0-9]*)?$/.test(str);
+  const isOperator = (str) => /^[+\-*/]$/.test(str);
+  const isVal = (str) => /^[0-9]$/.test(str);
   
   const onInputChange = (event) =>{
-    let value;
+    let value = event.target.value
+    const lastInput = value.at(-1)
+    let newVal = [...values]
+    let lastVal = newVal.at(-1)
 
-    // Если событие пришло от input
-    if (event.target.tagName === 'INPUT') {
-      value = event.target.value;
-    }
-    // Если событие пришло от button
-    else if (event.currentTarget.tagName === 'BUTTON') {
-      value = event.currentTarget.value === 'del' ? inputValue.slice(0, -1) : inputValue + event.currentTarget.value; // Добавляем значение кнопки к текущему значению
-    }
-
-    const lastVal = value[value.length - 1]; // Последний введённый символ
-
-    // Сброс ошибки
-    if (inputValue === "error") {
-      value = lastVal;
+    if(value.length < inputValue.length){
+      onDelete()
+      return
     }
 
-    // Обработка удаления символа
-    if (value.length < inputValue.length) {
-      if (isArithmetic(inputValue[inputValue.length - 1])) {
-        setOperator(null);
-      }
+    if(isVal(lastInput) || 
+      lastInput === '.' & isVal(lastVal.at(-1)) & lastVal.includes('.') === false || 
+      lastInput === '-' & lastVal.includes('-') === false & lastVal.length === 0 
+    ){
+      console.log('valid');
+      newVal[values.length - 1] += lastInput
+      setValue(newVal)
     }
-    // Обработка добавления оператора
-    else if (inputValue.length > 0 && isArithmetic(lastVal) && operator === null) {
-      setOperator(lastVal);
-    }
-    // Блокировка невалидных символов
-    else if (!isNum(lastVal)) {
-      return;
-    }
-
-    setInputValue(value);
+    else if(isOperator(lastInput) && isVal(inputValue.at(-1))){
+      setOperator([...operators, lastInput])
+      setValue([...values, ''])
+    }else return
+    
+    setInputValue((prev) => prev + lastInput)
+  }
+  const onDelete = () =>{
+    console.log('remove');
+    
   }
 
   const calculate = () =>{
-    if(operator === null || inputValue.indexOf(operator) === inputValue.length - 1) return
-    const parts = inputValue.split(operator)
-    let a = parseFloat(parts[0])
-    let b = parseFloat(parts[1])
-    let result = 0
-    switch (operator) {
-      case "+":
-        result = a + b
-        break;
-      case "-":
-        result = a - b
-        break;
-      case "*":
-        result = a * b
-        break;
-      case "/":
-        result = b === 0 ? "error" : a / b
-        break;
-      default:
-        break;
-    }
-    setOperator(null)
-    setInputValue(`${result}`)
+    // if(operator === null || inputValue.indexOf(operator) === inputValue.length - 1) return
+    // const parts = inputValue.split(operator)
+    // let a = parseFloat(parts[0])
+    // let b = parseFloat(parts[1])
+    // let result = 0
+    // switch (operator) {
+    //   case "+":
+    //     result = a + b
+    //     break;
+    //   case "-":
+    //     result = a - b
+    //     break;
+    //   case "*":
+    //     result = a * b
+    //     break;
+    //   case "/":
+    //     result = b === 0 ? "error" : a / b
+    //     break;
+    //   default:
+    //     break;
+    // }
+    // setOperator(null)
+    // setInputValue(`${result}`)
+  }
+
+  const resetCalc = () =>{
+    setInputValue(" ")
+    setValue([""])
+    setOperator([])
   }
 
   return (
@@ -80,7 +81,7 @@ function App() {
       <div className='container'>
         <Header theme={theme} toggleTheme={toggleTheme}/>
         <CalcField theme={theme} inputValue={inputValue} onInputChange={onInputChange} calc={calculate}/>
-        <CalcBtns theme={theme} calc={calculate} onInputChange={onInputChange} reset={() => setInputValue(" ")}/>
+        <CalcBtns theme={theme} calc={calculate} onInputChange={onInputChange} reset={resetCalc} remove={onDelete}/>
       </div>
     </div>
   );
